681557c42b588912b0635c8b20701219c7c8ffaa
sirAges <ekele19stephen96u@gmail.com>
2024-05-30 15:01:59 +0100

    wascra

--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -2,7 +2,8 @@
 import { useState } from "react";
 const SectionTwo = () => {
     const [numberList, setNumberList] = useState({
-        numbers: ""
+        numbers: "",
+        prefix:""
     });
     const formData = [
         {
@@ -11,6 +12,13 @@ const SectionTwo = () => {
             type: "text",
             label: "Numbers",
             placeholder: "list of numbers"
+        },
+        {
+            id: "prefix",
+            name: "prefix",
+            type: "text",
+            label: "prefix",
+            placeholder: "enter prefix"
         }
     ];
 
@@ -39,7 +47,7 @@ const SectionTwo = () => {
                     /(\d{3})(\d{3})(\d{4})/,
                     "$1-$2-$3"
                 );
-                const fullName = `AF${number}`;
+                const fullName = `${numberList.prefix}${number}`;
 
                 const vCard = `BEGIN                 :VCARD
                               VERSION:2.1
@@ -80,7 +88,7 @@ const SectionTwo = () => {
 
     return (
         <section
-            className="md:px-8 flex flex-1 flex-col w-full min-h-screen items-center space-y-6"
+            className="bg-white md:px-8 flex flex-1 flex-col w-full min-h-screen items-center space-y-6"
             id="#4"
         >
             <div className="flex flex-col space-y-6 md:w-1/2 items-center">
@@ -105,7 +113,18 @@ const SectionTwo = () => {
                                 className=" min-w-full bg-primary/10 px-5 md:px-4 py-4
                         rounded-lg"
                             >
-                                <textarea
+                                {id==="prefix"?<input
+                                    disabled={loading}
+                            
+                                    value={numberList[id]}
+                                    onChange={handleInput}
+                                    className="outline-none border-none
+                                    bg-transparent placeholder:text-primary/30"
+                                    id={id}
+                                    type={type}
+                                    name={name}
+                                    placeholder={placeholder}
+                                />:<textarea
                                     disabled={loading}
                                     rows={10}
                                     value={numberList[id]}
@@ -116,7 +135,7 @@ const SectionTwo = () => {
                                     type={type}
                                     name={name}
                                     placeholder={placeholder}
-                                />
+                                />}
                             </div>
                         </div>
                     ))}
