93599f188dfa0d690ac84fde4ed73660d463fd38
sirAges <ekele19stephen96u@gmail.com>
2024-06-13 15:41:26 +0100

    channel003

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,18 +9,18 @@ const Header = ({ dropdown, setDropdown }) => {
     const [cat, setCat] = useState(null);
     const [search, setSearch] = useState("");
     const menus = [
-    "Business",
-    "Crime",
-    "Entertainment",
-    "Opinion",
-    "Podcast",
-    "Politics",
-    "Programmes",
-    "Sport",
-    "Tech",
-    "World",
-    "More"
-];
+        "Business",
+        "Crime",
+        "Entertainment",
+        "Opinion",
+        "Podcast",
+        "Politics",
+        "Programmes",
+        "Sport",
+        "Tech",
+        "World",
+        "More"
+    ];
     return (
         <div
             className="flex flex-col w-full  items-end
